8296eef994f5e90f25736c6c058a9731614c36b7
Habib <habiburrahman10224@gmail.com>
2025-03-16 01:55:05 +0600

    added a conditional file

---
+++ condition.js
@@ -0,0 +1,40 @@
+/**
+ 
+//  Define Age and Subscription Status
+let age = 20;
+let hasSubscription = true;
+if (age >= 18 && hasSubscription) {
+    console.log("Access granted.");
+} else {
+    console.log("Access denied.");
+}
+* 
+ */
+
+
+/**
+
+
+// Example 2: Checking if a Number is Divisible by 3 or 5
+
+let num = 15;
+if (num % 3 === 0 || num % 5 === 0) {
+    console.log("The number is divisible by 3 or 5.");
+} else {
+    console.log("The number is not divisible by 3 or 5.");
+}
+ * 
+ */
+
+/**
+ 
+// Example 3: Checking Login Credentials (Username & Password)
+let username = "habib";
+let password = "1234";
+if (username === "habib" && password === "1234") {
+    console.log("Login successful.");
+} else {
+    console.log("Invalid username or password.");
+}
+* 
+ */
